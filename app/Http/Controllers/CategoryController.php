51392df2a1589397aa08ab190debbe4d8e44977c
magiksen <elemejore@gmail.com>
2022-06-10 10:14:26 -0400

    Eloquent ORM read data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,9 @@ use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
     public function AllCat() {
-        return view('admin.category.index');
+        //$categories = Category::all();
+        $categories = Category::latest()->get();
+        return view('admin.category.index', compact('categories'));
     }
 
     public function AddCat(Request $request) {
@@ -25,24 +27,24 @@ class CategoryController extends Controller
         ]);
 
         // ORM #1
-        // Category::insert([
-        //     'category_name' => $request->category_name,
-        //     'user_id' => Auth::user()->id,
-        //     'created_at' => Carbon::now()
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
 
-        // ]);
+        ]);
 
-        //ORM #2
+        //ORM #2 esta es al de la documentacion official 9
         // $category = new Category;
         // $category->category_name = $request->category_name;
         // $category->user_id = Auth::user()->id;
         // $category->save();
 
-        //Query Builder
-        $data = array();
-        $data['category_name'] = $request->category_name;
-        $data['user_id'] = Auth::user()->id;
-        DB::table('categories')->insert($data);
+        // //Query Builder
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id'] = Auth::user()->id;
+        // DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success', 'Category inserted successfull');
 
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -22,13 +22,27 @@
                             <thead>
                             <tr>
                                 <th scope="col">SL No</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
+                                <th scope="col">Category Name</th>
+                                <th scope="col">User Name</th>
                                 <th scope="col">Create At</th>
                             </tr>
                             </thead>
                             <tbody>
-
+                                @php($i = 1)
+                                @foreach($categories as $category)
+                                <tr>
+                                    <th scope="row">{{ $i++ }}</th>
+                                    <td>{{ $category->category_name }}</td>
+                                    <td>{{ $category->user_id }}</td>
+                                    <td>
+                                        @if($category->created_at == NULL)
+                                        <span class="text-danger">No Date Set</span>
+                                        @else
+                                        {{ $category->created_at->diffForHumans() }}
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
